3a01963570e89cc162f3c2f8bfb369d9b71d2e61
Elijah <ilyasmolnikov567@gmail.com>
2023-10-22 01:57:53 +0300

    Added buffer exhaust test

--- src/test/java/edu/project1/TextFileWordProviderTest.java
+++ src/test/java/edu/project1/TextFileWordProviderTest.java
@@ -17,11 +17,27 @@ public class TextFileWordProviderTest {
     }
 
     @Test
-    @DisplayName("Test valid file path with unacceptable words")
-    void tesUnacceptableWordDictionary() {
+    @DisplayName("Test valid file path with unacceptable words with random")
+    void tesUnacceptableWordDictionaryWithRandom() {
         assertThatThrownBy(() -> {
             TextFileWordProvider provider = new TextFileWordProvider(true,
                 "src/test/resources/project1testresources/UnacceptableWordDictionary.txt");
+            provider.getRandomWord();
+        })
+            .isInstanceOf(EmptyWordsStockException.class)
+            .hasMessage("\nWords stock is empty.")
+            .hasCauseInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    @DisplayName("Test words buffer exhaust")
+    void testWordsBufferExhaust() {
+        assertThatThrownBy(() -> {
+            TextFileWordProvider provider = new TextFileWordProvider(false,
+                "src/test/resources/project1testresources/UnacceptableWordDictionary.txt");
+            provider.getRandomWord();
+            provider.getRandomWord();
+            provider.getRandomWord();
             provider.getRandomWord();
         })
             .isInstanceOf(EmptyWordsStockException.class)
