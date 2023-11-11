e90c6e4c83a9065fb0ddb0ed4f222e162b05a70e
Elijah <ilyasmolnikov567@gmail.com>
2023-11-11 23:20:52 +0300

    Task 6: removed SuppressWarning annotation

--- src/main/java/edu/hw5/Task6.java
+++ src/main/java/edu/hw5/Task6.java
@@ -7,12 +7,12 @@ public final class Task6 {
 
     private final static String NULL_STRING_MESSAGE = "String arg can't be null.";
     private final static String EMPTY_BLANK_MESSAGE = "String arg can't have length of 0 or contains only whitespaces";
+    private final static String WORD_BOUNDARY_ANCHOR = "\\b";
 
     private Task6() {
 
     }
 
-    @SuppressWarnings("all")
     public static boolean isFirstArgIsSubsequenceOfSecond(String first, String second) {
         validation(first, second);
         StringBuilder builder = new StringBuilder();
@@ -21,7 +21,8 @@ public final class Task6 {
             builder.append(first.charAt(i));
         }
         builder.append(".*");
-        Pattern matchingPattern = Pattern.compile("\\b" + builder + "\\b", Pattern.DOTALL);
+        Pattern matchingPattern =
+            Pattern.compile(WORD_BOUNDARY_ANCHOR + builder + WORD_BOUNDARY_ANCHOR, Pattern.DOTALL);
         Matcher matcher = matchingPattern.matcher(second);
         return matcher.find();
     }
