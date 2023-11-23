f06fe77393afe7d7ecdc53c9862193eb1ded0944
Elijah <ilyasmolnikov567@gmail.com>
2023-11-23 22:25:55 +0300

    Test class renaming

--- src/test/java/edu/project3/UnusualBehaviourTest.java
+++ src/test/java/edu/project3/UnusualBehaviourTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class LocalFileUnusualBehaviourTest {
+class UnusualBehaviourTest {
 
     @Test
     @DisplayName("Unexisting file test.")
