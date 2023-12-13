1d40e7282cdb4862ec0b7c48def407e16dd32a35
Elijah <ilyasmolnikov567@gmail.com>
2023-12-13 23:39:29 +0300

    Task3 FINALLY

--- src/test/java/edu/hw11/task2/Task2Test.java
+++ src/test/java/edu/hw11/task2/Task2Test.java
@@ -1,10 +1,10 @@
 package edu.hw11.task2;
 
-
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.InvocationTargetException;
 import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.dynamic.DynamicType;
-import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
 import net.bytebuddy.implementation.MethodDelegation;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -15,9 +15,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Task2Test {
 
+    private static Class<?> clazz;
+    private static Object instance;
+
     static class ArithmeticUtilsInterceptor {
 
-        public static int multiply(int a, int b) {
+        public int sum(int a, int b) {
             return a * b;
         }
 
@@ -25,15 +28,23 @@ class Task2Test {
 
     @BeforeAll
     public static void setup() {
-        ByteBuddyAgent.install();
         try (
             DynamicType.Unloaded<?> unloaded = new ByteBuddy()
-                .redefine(ArithmeticUtils.class)
+                .subclass(ArithmeticUtils.class)
                 .method(named("sum"))
-                .intercept(MethodDelegation.to(ArithmeticUtilsInterceptor.class))
+                .intercept(MethodDelegation.to(new ArithmeticUtilsInterceptor()))
                 .make()
         ) {
-            unloaded.load(unloaded.getClass().getClassLoader(), ClassReloadingStrategy.fromInstalledAgent()).getLoaded();
+            clazz = unloaded.load(
+                unloaded.getClass().getClassLoader(),
+                ClassLoadingStrategy.UsingLookup.of(
+                    MethodHandles.privateLookupIn(ArithmeticUtils.class, MethodHandles.lookup())
+                )
+            ).getLoaded();
+            instance = clazz.getDeclaredConstructor().newInstance();
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -51,9 +62,12 @@ class Task2Test {
     @ParameterizedTest
     @MethodSource("provideTestMethod")
     @DisplayName("Test method original call and interceptor calls.")
-    void testOriginalMethod(int value1, int value2, int expectedMultiply) {
+    void testOriginalMethod(int value1, int value2, int expectedMultiply)
+        throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        //When
+        int actual = (int) clazz.getMethod("sum", int.class, int.class).invoke(instance, value1, value2);
         //Then
-        assertThat(ArithmeticUtils.sum(value1, value2)).isEqualTo(expectedMultiply);
+        assertThat(actual).isEqualTo(expectedMultiply);
     }
 
 }
